f4ff664a19f9ce9d2298871121bf1161917a2733
Scott Mobley-Schreibman <smobley62@gmail.com>
2015-01-01 10:05:28 -0800

    Added parse.com SDK.

---
+++ app/src/main/java/com/scottmobleyschreibman/ribbit/RibbitApplication.java
@@ -0,0 +1,22 @@
+package com.scottmobleyschreibman.ribbit;
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Owner on 1/1/2015.
+ */
+public class RibbitApplication extends Application {
+    @Override
+    public  void onCreate(){
+        super.onCreate();
+    Parse.enableLocalDatastore(this);
+
+    Parse.initialize(this, "271mH3IB373oi7QvQphsRBjB3wvBaIoAYyrJwAKn", "i33rqcUudqgmfUiA90uyiHZQAHwFqUh1iyTUH2eA");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
