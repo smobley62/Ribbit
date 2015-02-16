cfbd41ec766882f1b36d3221cfef98752442bc4f
Scott Mobley-Schreibman <smobley62@gmail.com>
2015-02-16 07:25:56 -0800

    Customized action bar

--- app/src/main/java/com/scottmobleyschreibman/ribbit/MainActivity.java
+++ app/src/main/java/com/scottmobleyschreibman/ribbit/MainActivity.java
@@ -215,7 +215,7 @@ public class MainActivity extends FragmentActivity implements
             // the TabListener interface, as the callback (listener) for when
             // this tab is selected.
             actionBar.addTab(actionBar.newTab()
-                    .setText(mSectionsPagerAdapter.getPageTitle(i))
+                    .setIcon(mSectionsPagerAdapter.getIcon(i))
                     .setTabListener(this));
         }
     }
